f412af4aec53b94312bf1a946c3b0be578f2197e
Saneyuki Tadokoro <post@saneyuki.gfunction.com>
2012-12-30 08:51:46 +0900

    Add from method test

--- test.js
+++ test.js
@@ -1,16 +1,29 @@
-/*global window, Modus*/
+/*global console, setTimeout, Modus*/
 
 (function () {
 
   "use strict";
   
   Modus.modules.log = function (message) {
-    window.console.log(message);
+    console.log(message);
   };  
+  
+  
+  Modus.modules.test = {
+    timer: function (delay) {
+      setTimeout(function () {
+        console.log('Timeout!');
+      }, delay);
+    }
+  };
+  
   
   var $ = new Modus.Namespace();
   
-  $.imports('log').as('out');
+  $.imports('log').as('out')
+   .imports('timer').from('test');
+   
   $.out('Hello, world');
+  $.timer(2000);
     
 }());
